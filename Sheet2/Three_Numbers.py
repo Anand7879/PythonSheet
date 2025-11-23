12936136b8ffa94a7cbb3d6383a995edc650ff00
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-23 21:15:19 +0530

    contest 2

--- Sheet2/Three_Numbers.py
+++ Sheet2/Three_Numbers.py
@@ -1,10 +1,10 @@
-K,S = map(int,input().split())
+K, S = map(int, input().split())
 count = 0
 
-for i in range(0,K):
-    for j in range(0,K):
-        for k in range(0,K):
-            if(i+j+k==S):
-                count+=1
+for i in range(K+1):
+    for j in range(K+1):
+        k = S - i - j
+        if 0 <= k <= K:
+            count += 1
 
 print(count)
