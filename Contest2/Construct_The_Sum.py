7a35f7d2eb9f29ebd11d2a028776d0a8e97e408f
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-23 20:53:52 +0530

    contest 2

--- Contest2/Construct_The_Sum.py
+++ Contest2/Construct_The_Sum.py
@@ -1,4 +1,24 @@
 T = int(input())
 
-for i in range(T):
+for _ in range(T):
     n,s = map(int,input().split())
+    
+    max_sum = n * (n + 1) // 2
+    if s > max_sum:
+        print(-1)
+        continue
+
+    ans = list()
+    x = n
+    while x>0 and s>0:
+
+        if(x<=s):
+            ans.append(x)
+            s-=x
+
+        x-=1
+    if(s>0):
+        print(-1)
+    else:
+        print(*ans)        
+            
