8dcaf2863949d0f7bc6b073082a66434c2041b45
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-24 16:22:36 +0530

    Sheet 3

--- Sheet3/Lowest_Number.py
+++ Sheet3/Lowest_Number.py
@@ -4,10 +4,10 @@ T = list(map(int,input().split()))
 lowest = T[0]
 index = 1
 
-for i in range(2,N+1):
+for i in range(1,N):
 
     if(T[i]<lowest):
         lowest = T[i]
-        index= i
+        index= i+1
 
 print(lowest,index)
