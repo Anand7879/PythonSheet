30f4ed94f881d36274785edc4c1bad3cc683a398
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-25 16:08:18 +0530

    Post Not Upload without login

--- Sheet3/Sortng.py
+++ Sheet3/Sortng.py
@@ -1,11 +1,17 @@
 N = int(input())
 arr = list(map(int, input().split()))
 
-for i in range(N):
+for i in range(0,N):
     min_index = i
     
     for j in range(i+1,N):
-        if(arr[min_index]>arr[j]):
+        if(arr[j]<arr[min_index]):
             min_index = j
+        
+    if min_index != i:
+        arr[min_index], arr[i] = arr[i], arr[min_index]
+
+print(*arr)            
+
 
 
