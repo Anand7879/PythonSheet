d978401da7ad6f8e194c776c72a0eaf041164e90
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-26 16:21:21 +0530

    Sheet3

--- Sheet3/Max_SubArray.py
+++ Sheet3/Max_SubArray.py
@@ -2,4 +2,8 @@ T = int(input())
 for i in range(T):
     N = int(input())
     arr = list(map(int, input().split()))
-    
+    Max_list = arr
+    step = 1
+    for i in range(0,step):
+        Max_list.append()
+
