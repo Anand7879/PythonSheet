263cdc2d6aa5717e77e409f88ccc5b6f788fe25f
Anand7879 <Anand7879@users.noreply.github.com>
2025-12-09 16:22:56 +0530

    Sheet3

--- Sheet3/Minimize_Number.py
+++ Sheet3/Minimize_Number.py
@@ -1,6 +1,12 @@
 N = int(input())
 arr = list(map(int,input().split()))
 arr1 = []
-if(all ( x % 2==0 for x in arr)):
-    arr = all ( x % 2==0 for x in arr)
-    print(arr)
+count = 0
+while(True):
+    if(all ( x % 2==0 for x in arr)):
+        arr = [x // 2 for x in arr]
+        count+=1
+    else:
+        break
+
+print(count)        
